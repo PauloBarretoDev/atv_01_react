509554943524a0e927b49e842020b8e8624c942e
Paulo Barreto Neto <105065779+PauloBarretoDev@users.noreply.github.com>
2023-08-31 22:31:08 +0000

    consumindoApiDeEndereco

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+// MyApp.js
+import React from 'react';
+import EnderecoComponent from './EnderecoComponent'; // Importe o componente aqui
+
+function MyApp() {
+    return (
+        <div>
+            <h1>Minha Aplicação</h1>
+            <EnderecoComponent /> {/* Use o componente aqui */}
+        </div>
+    );
+}
+
+export default MyApp;
